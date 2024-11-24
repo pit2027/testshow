0749e894048ae3e827b39716866bdc3b455d4a84
pit <pitnic2027@gmail.com>
2024-11-24 19:19:27 +0300

    counter()

--- main.py
+++ main.py
@@ -6,9 +6,22 @@ def number():
         return all_number
     return inner
 
+def counter():
+    count = 0
+    def inner(val):
+        nonlocal count
+        count+=val
+        return count
+    return inner
+
+
 if __name__ == '__main__':
     f = number()
+    c = counter()
     print(f(1))
     print(f(1))
     print(f(1))
+    print(c(1))
+    print(c(1))
+    print(c(1))
 
